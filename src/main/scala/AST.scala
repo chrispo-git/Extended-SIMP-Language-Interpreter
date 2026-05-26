1e9d945d5c7497d8d360cf55669a13916b0235d8
chrispo <haasc865@gmail.com>
2026-05-26 18:07:24 +0100

    Create initial AST structure

---
+++ src/main/scala/AST.scala
@@ -0,0 +1,49 @@
+package simp
+
+// AST Structure of SIMP
+
+/* Integer Expressions can either be:
+    - Integer Value
+    - Dereferenced Variable
+    - 2 Expressions joined by a Binary Operator
+*/
+enum Expr:
+    case Deref(loc: String)
+    case Num(value: Int)
+    case BinaryOp(l: Expr, op: Op, r: Expr)
+
+// Operators allowed - Add, Sub, Mul, Div
+enum Op:
+    case Add, Sub, Mul, Div
+
+
+/* Boolean Expressions can either be:
+    - True
+    - False
+    - E bop E
+    - ¬B
+    - B^B
+    - BvB
+*/
+enum BoolExpr:
+    case Literal(value: Boolean)
+    case Compare(l: Expr, bop: Bop, r: Expr)
+    case Not(expr: BoolExpr)
+    case And(l: BoolExpr, r: BoolExpr)
+    case Or(l: BoolExpr, r: BoolExpr)
+
+enum Bop:
+    case Gt, Lt, Eq, Gte, Lte
+
+enum Cmd:
+    case Skip
+    case Assign(loc: String, expr: Expr)
+    case Seq(fst: Cmd, snd: Cmd)
+    case If(cond: BoolExpr, thenBranch: Cmd, elseBranch: Cmd)
+    case While(cond: BoolExpr, body: Cmd)
+
+enum Program:
+    case PCmd(cmd: Cmd)
+    case PExpr(expr: Expr)
+    case PBool(boolExpr: BoolExpr)
+
