4c0c09fb0b61ebd3038c4f1d1a945f8d59b292c6
chrispo <haasc865@gmail.com>
2026-06-16 09:59:23 +0100

    Move parseParams

--- src/main/scala/simp/Parser/ParserFunctions.scala
+++ src/main/scala/simp/Parser/ParserFunctions.scala
@@ -1,33 +1,6 @@
 package simp
 
 trait ParserFunctions { self: Parser =>
-    protected def parseParams(): List[(String, SimpType)] = {
-        expect(Token.OpenBracket)
-        if peek() == Token.CloseBracket then {
-            advance()
-            List()
-        } else {
-            val params = scala.collection.mutable.ListBuffer[(String, SimpType)]()
-            params += parseParam()
-            while peek() == Token.Comma do {
-                advance()
-                params += parseParam()
-            }
-            expect(Token.CloseBracket)
-            params.toList
-        }
-    }
-    protected def parseParam(): (String, SimpType) = {
-        peek() match {
-            case Token.Variable(name) => {
-                advance()
-                expect(Token.Colon)
-                val t = parseType()
-                (name, t)
-            }
-            case x => throwError(s"Expected parameter name, got '$x'")
-        }
-    }
     protected def parseArgs(): List[Expr] = {
         expect(Token.OpenBracket)
         if peek() == Token.CloseBracket then {
